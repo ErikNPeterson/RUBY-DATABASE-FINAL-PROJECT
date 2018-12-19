de3d28386b72b182bd17424a82f55b6a7e176ea3
Erik Peterson <erikeh1@gmail.com>
2018-12-18 20:43:59 -0500

    started creating and testing controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  resources :users
+  # resources :users do 
+  #   resources :events, except: [:index]
+  # end
+
+
+  # resources :events
+  # get '/events' => 'events#index'
+  # get '/events/:id' => 'events#show'
+  # post '/events' => 'events#create'
+  # put '/events/:id' => 'events#update'
+  # delete '/events/:id' => 'events#delete'
+
+  resources :events, only: [:show, :index] do
+    resources :messages, only: [:index, :create]
+  end
+
+
 end
+
+  #  # these routes are for showing users a login form, logging them in, and logging them out.
+  #  get '/login' => 'sessions#new'
+  #  post '/login' => 'sessions#create'
+  #  get '/logout' => 'sessions#destroy'
+  #     # These routes will be for signup. The first renders a form in the browse, the second will 
+  #   # receive the form and create a user in our database using the data given to us by the user.
+  #   get '/signup' => 'users#new'
+  #   post '/users' => 'users#create'
